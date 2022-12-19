ebd460ec86a1e023fba610aa8e859d86f4217865
rodrvn <rodrigojvallejos@gmail.com>
2022-12-19 13:46:35 -0300

    conectamos basededatos y se visualiza en html

--- Analisis/app.py
+++ Analisis/app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
-app =Flask(__name__)
+app = Flask(__name__)
+
+#conect database
+def conectar():
+    db = sqlite3.connect('database.db')
+    cursor = db.cursor()
+    return db, cursor
+
+#cerrar conexion
+def cerrar(conn):
+    conn.commit()
+    conn.close()
+
+# conectamos a la base y guardamos en una lista
+def recibir_clientes():
+    db, cursor = conectar()
+    cursor.execute("SELECT * FROM clientes")
+    clientes = (cursor.fetchall())
+    cerrar(db)
+    return clientes
+
+
 
 @app.route('/')
-def index():
-    return "hola mundo!"
+def clientes():
+    clientes = recibir_clientes()
+    return render_template("lista.html", clientes=clientes)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(debug=True,port=9000)
